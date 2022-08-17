b5acaad6f1e86c318e3f9ab3d735b64546649bb4
MoGAeon <rimatzkijo@yahoo.de>
2022-08-17 02:53:12 +0200

    repait

--- AudioSafety.py
+++ AudioSafety.py
@@ -13,42 +13,9 @@ import datetime
 import moviepy.editor as mp
 from gtts import gTTS
 
-argslen = len(sys.argv)
-
-inputFile = ""
-
-if argslen == 1:
-    print("Enter a filename:")
-    inputFile = input()
-else:
-    inputFile = sys.argv[1]
-
-
-censorType = -1
-
-if argslen == 3:
-    censorType = sys.args[2]
-elif argslen == 1:
-    print("Enter censor type:")
-    print("-t: Text To Speech")
-    print("-b: beep")
-    print("-s: Silence")
-    censorType = input().replace(" ", "")
-    
-if (censorType == "-t"):
-    censorType = 0
-elif (censorType == "-b"):
-    censorType = 1
-elif (censorType == "-s"):
-    censorType = 2
-    
-
-times = []
-
-times.append(("Start", time.time()))
-
 # Forbidden words
 badWords = pd.read_csv("BadWords.txt", header=None, names=["word"])
+censorType = 1
 
 # Vosk and ffmpeg setup
 SetLogLevel(-1)
@@ -57,14 +24,12 @@ model = Model(lang="en-us")
 rec = KaldiRecognizer(model, sample_rate)
 rec.SetWords(True)
 
-times.append(("Setup done", time.time()))
-
 # List of words with Timestamps
 transcript = pd.DataFrame(columns = ["conf", "end", "start", "word"])
 idx = 0
 
 process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
-                            sys.argv[1],
+                            "Test.mp4",
                             '-ar', str(sample_rate) , '-ac', '1', '-f', 's16le', '-'],
                             stdout=subprocess.PIPE)
 
@@ -80,8 +45,6 @@ while True:
                 if i["word"] != "":
                     transcript.loc[idx] = i
                     idx += 1
-                    
-times.append(("STT done", time.time()))
 
 if not os.path.exists("BadWords"):
     os.makedirs("BadWords")
@@ -92,8 +55,6 @@ existingFiles = os.listdir("BadWords")
 
 video = mp.VideoFileClip("Test.mp4")
 audio = video.audio
-
-times.append(("Video loaded", time.time()))
 
 start = datetime.timedelta()
 rx = re.compile(badWordJoin)
@@ -111,7 +72,7 @@ for idx, row in transcript.iterrows():
     duration = audio.subclip(str(end), str(start)).duration
     
     # TTS-Replacement
-    if (censorType == 0):
+    if (censorType != 1):
         word = row["word"].lower()
         filename = word.replace(" ", "") + ".mp3"
         
@@ -119,33 +80,19 @@ for idx, row in transcript.iterrows():
             outObj = gTTS(word.lower())
             outObj.save("BadWords/"+ filename)
             existingFiles.append(filename)
-    
+
         replacementAudio = mp.AudioFileClip("BadWords/"+ filename)
         clips.append(replacementAudio.fx(mp.vfx.speedx, replacementAudio.duration / duration))
     # Beep-Replacement
-    elif (censorType == 1):
+    if (censorType == 1):
         while (duration >= 5):
             clips.append(mp.AudioFileClip("beep.mp3"))
             duration = duration - 5
         clips.append(mp.AudioFileClip("beep.mp3").set_duration(duration))
-    else:
-        clips.append(mp.AudioFileClip("beep.mp3").set_duration(duration))
-    
 
-times.append(("Audio generation done", time.time()))
 
 audio = mp.concatenate_audioclips(clips)
 
-times.append(("Audio merge done", time.time()))
-
 video = video.set_audio(audio)
 
-times.append(("Audio on video replaced", time.time()))
-
-video.write_videofile(sys.argv[1])
-
-times.append(("Export done", time.time()))
-
-prev_ts = times[0][1]
-for text, ts in times:
-    print(text, str(ts - prev_ts))
+video.write_videofile("output.mp4")
