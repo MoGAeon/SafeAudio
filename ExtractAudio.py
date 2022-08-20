cb5c9870fa82fc95232494a1d6ff625cc2244dea
MoGAeon <rimatzkijo@yahoo.de>
2022-08-20 02:13:11 +0200

    Delete ExtractAudio.py

--- ExtractAudio.py
+++
@@ -1,7 +0,0 @@
-import os
-import moviepy.editor as mp 
-
-for file in os.listdir("input"):
-    video = mp.VideoFileClip("input/" + file)
-    video.audio.write_audiofile("input/" + file + ".mp3")
-
